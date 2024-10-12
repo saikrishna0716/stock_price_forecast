46f0830496c68aea79718cf7a696c77fe2c31638
Sesha Sai Krishna <81363740+saikrishna0716@users.noreply.github.com>
2024-10-11 23:27:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import pandas as pd
-from prophet import Prophet
-import streamlit as st
-
-# File uploader to allow users to upload their stock price data
-# uploaded_file = st.file_uploader('/Users/saikrishna/Desktop/Stock Price Predeictions/stock_prices.csv', type='csv')
-
 import streamlit as st
 
 st.title('My Web Application')
