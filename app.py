341ac9b0f623d3e6695ab6c2740a1b317cee7c4c
Sesha Sai Krishna <81363740+saikrishna0716@users.noreply.github.com>
2024-10-12 12:50:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+from prophet import Prophet
+import streamlit as st
 
-st.write("Hello World")
-st.write("## This is a H2 Title!")
+# File uploader to allow users to upload their stock price data
+# uploaded_file = st.file_uploader('/Users/saikrishna/Desktop/Stock Price Predeictions/stock_prices.csv', type='csv')
+
+# Streamlit app title
+st.title('Stock Price Prediction')
+
+# df = pd.DataFrame({'ds':['2021-01-01','2021-02-01','2021-03-01','2021-04-01','2021-05-01','2021-06-01','2021-07-01','2021-08-01'],
+#                    'y':[10,20,30,35,35,40,70,100]})
+
+df = pd.read_csv('stock_prices.csv')
+
+# Display the data
+# print(df.head())
+# Display the data
+st.write('Stock Price Data', df.head())
+
+# Fit the Prophet model
+model = Prophet()
+model.fit(df)
+
+# Forecast for the next month
+future = model.make_future_dataframe(periods=1, freq='M')
+forecast = model.predict(future)
+
+st.write('Next Month Forecast', forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+# Plot the forecast
+fig = model.plot(forecast)
+st.pyplot(fig)
